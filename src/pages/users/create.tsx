442e304c56f94dedd30326a6a91adfd0d5c23685
Gabriel Campos <caampos22@gmail.com>
2022-06-27 04:36:08 -0300

    :sparkles: finish app skeleton

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react"
+import Input from "../../components/Form/Input"
+
+import Header from "../../components/Header"
+import Sidebar from "../../components/Sidebar"
+
+
+const CreateUser = () => {
+  return (
+    <Box>
+      <Header />
+
+      <Flex
+        width="100%"
+        my="1.5rem"
+        maxWidth="1480px"
+        mx="auto"
+        px="1.5rem"
+      >
+        <Sidebar />
+        <Box
+          flex="1"
+          borderRadius="8px"
+          bg="gray.800"
+          p="2rem"
+        >
+          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+
+          <Divider my="1.5rem" borderColor="gray.700" />
+          
+          <VStack spacing="2rem">
+            <SimpleGrid minChildWidth="240px" spacing="2rem" w="100%">
+              <Input name="name" label="Nome completo" />
+              <Input name="email" type="email" label="E-mail" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing="2rem" w="100%">
+              <Input name="password" type="password" label="Senha" />
+              <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="2rem" justify="flex-end">
+            <HStack spacing="1rem">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default CreateUser
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,91 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react"
+import { RiAddLine, RiPencilLine } from "react-icons/ri"
+
+import Header from "../../components/Header"
+import Pagination from "../../components/Pagination"
+import Sidebar from "../../components/Sidebar"
+
+
+const UserList = () => {
+  return (
+    <Box>
+      <Header />
+
+      <Flex
+        width="100%"
+        my="1.5rem"
+        maxWidth="1480px"
+        mx="auto"
+        px="1.5rem"
+      >
+        <Sidebar />
+        <Box
+          flex="1"
+          borderRadius="8px"
+          bg="gray.800"
+          p="2rem"
+        >
+          <Flex mb="2rem" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20px" />}
+            >
+              Criar novo Usuários
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="1.5rem" color="gray.300" width="2rem">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>
+                  Usuário
+                </Th>
+                <Th>
+                  Data de cadastro
+                </Th>
+                <Th width="2rem"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="1.5rem">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Gabriel Campos</Text>
+                    <Text fontSize="sm" color="gray.300">caampos22@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>04 de abril de 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16px" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default UserList
